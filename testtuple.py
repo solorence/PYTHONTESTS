6c14ab44d876b53111f2384ec7c1677dd413f4c6
solorence <doc.electro1@gmail.com>
2026-09-11 14:08:01 +0100

    latest update made on testtuple.py

--- testtuple.py
+++ testtuple.py
@@ -6,9 +6,72 @@ John, 23, 78
 Mary, 21, 65"""
 
 listOfStudentInfo=[]
+record={}
+studentNames=[]
+studentsByScoreCategory={}
 
 for student in students.split('\n'):
  listOfStudentInfo.append(tuple(student.split(', ')))
 
-print( listOfStudentInfo)
+def removeDuplicatesInList(LIST):
+ duplicatesIndex=[]
+ compared = {}
+ for data in LIST:  
+  compared.setdefault(data, False)
+  seen = False
+  for dataindex in range(len(LIST)):   
+   if data == LIST[dataindex] and not compared[data]:
+    if seen == True:     
+     duplicatesIndex.append(dataindex)
+     compared[data]= True
+    seen = True
+ 
+ duplicatesIndex.reverse()
+
+ for i in duplicatesIndex:
+  del LIST[i]
+ 
+
+removeDuplicatesInList(listOfStudentInfo)
+
+def addToRecord(LIST):
+ for data in LIST:
+  name, age, score = list(data)
+  datainfo = list(data)
+  studentNames.append(name)
+
+  for info in datainfo:
+   record[name]={}
+   record[name]['name']= name
+   record[name]['age']= age
+   record[name]['score']= score
+
+
+addToRecord(listOfStudentInfo)
+
+def calAverageScore():
+ total=0
+ for name in studentNames:
+   total += int(record[name]['score'])
+ return total//len(studentNames)
+
+
+def studentWithHighestScore():
+ studentName=''
+ newHighScore=0
+ for name in studentNames:
+  if int(record[name]['score']) > newHighScore:
+   studentName = name
+   newHighScore = int(record[name]['score'])
+ 
+ return studentName   
+
+
+print('Average Score:', calAverageScore())
+
+print('The student with the highest Score is: ', studentWithHighestScore()   )
+
+
+
+
 
