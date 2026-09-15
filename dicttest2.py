a83b36f586ac45f0824dcea5d21b9733a7efa95c
solorence <doc.electro1@gmail.com>
2026-09-15 17:28:07 +0100

    developing dicttest2.py

--- dicttest2.py
+++ dicttest2.py
@@ -5,20 +5,43 @@ Paracetamol, 12, 1500
 Ibuprofen, 8, 2500
 Amoxicillin, 5, 3000"""
 
-salesProcessed = [{},{},{},{},{},{}]
-
 
 
 def analyzeSales(sales):
- productsInfo = sales.split('\n') 
- 
+ productSalesInfo = {}
+ productSalesCollection2 = []
  labels=['product', 'quantity', 'price']
+ productNames= ['Paracetamol', 'Amoxicillin', 'Vitamin C', 'Ibuprofen']
+
+ productSalesCollection1 = sales.split('\n') 
  
- {
-    'total_items_sold': {},
-    'total_sales': {},
-    'product_sales': {}
- }
+ for index in range(len(productSalesCollection1)):
+  productSalesCollection2.append((productSalesCollection1[index]).split(', '))
+
+ print(productSalesCollection2)
+ 
+ for prod in productSalesCollection2:
+  for name in productNames:
+   if name in prod:
+    productSalesInfo[name]={}
+    for data in prod:
+     print(data)
+     for i in range(len(labels)):
+      print(i)      
+      productSalesInfo[name][labels[i]]= data
+      print(productSalesInfo)   
+ 
+ print(productSalesInfo)
+ 
+
+ #{
+ #   'total_items_sold': {},
+ #  'total_sales': {},
+ #   'product_sales': {}
+ #}
+
+
+analyzeSales(sales)
 
  
 
